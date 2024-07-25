2fdee8b69ec4eb69b1f9e431e9287b2ed99c4b18
Dan Toms <danjtoms@protonmail.com>
2024-07-25 11:52:14 +0100

    Add function to get and display the name, image and summary of the returned shows

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 const form = document.querySelector('#searchForm');
-form.addEventListener('submit', function (e) {
+const shows = document.querySelector('#shows')
+form.addEventListener('submit', async function (e) {
     e.preventDefault();
+    shows.innerHTML = ""
+    const searchTerm = form.elements.query.value
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
+    console.log(res.data[0].show.name)
+    console.log(res.data[0])
+
+    for (result of res.data) {
+        const showContainer = document.createElement('DIV')
+        const img = document.createElement('IMG')
+        const name = document.createElement('H2')
+        const desc = document.createElement('P')
+        img.setAttribute('src', result.show.image.medium)
+        name.textContent = result.show.name
+        desc.textContent = result.show.summary
+        showContainer.append(img)
+        showContainer.append(name)
+        // showContainer.append(desc)
+        showContainer.innerHTML += result.show.summary
+        shows.append(showContainer)
+    }
+
+
 })
